5b63a43e8379a3e178fb468bcc0a951cdb4c0f10
Chao Qian <chao.qian@uni-due.de>
2021-10-25 15:40:39 +0200

    add new test file for bus scan

---
+++ app/i2c_bus_scan.c
@@ -0,0 +1,53 @@
+//
+// Created by chao on 25/10/2021.
+//
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+int main() {
+    // Enable UART so we can print status output
+    stdio_init_all();
+
+    // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
+    i2c_init(i2c0, 100 * 1000);
+    gpio_set_function(0, GPIO_FUNC_I2C);
+    gpio_set_function(1, GPIO_FUNC_I2C);
+    gpio_pull_up(0);
+    gpio_pull_up(1);
+    // Make the I2C pins available to picotool
+    // bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+
+    while (1)
+    {
+        char c = getchar_timeout_us(10000);
+        if (c == 't')
+        {
+
+
+            printf("\nI2C Bus Scan\n");
+            printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+
+            for (int addr = 0; addr < (1 << 7); ++addr) {
+                if (addr % 16 == 0) {
+                    printf("%02x ", addr);
+                }
+
+                // Perform a 1-byte dummy read from the probe address. If a slave
+                // acknowledges this address, the function returns the number of bytes
+                // transferred. If the address byte is ignored, the function returns
+                // -1.
+
+                // Skip over any reserved addresses.
+                int ret;
+                uint8_t rxdata;
+                ret = i2c_read_blocking(i2c0, addr, &rxdata, 1, false);
+
+                printf(ret < 0 ? "." : "@");
+                printf(addr % 16 == 15 ? "\n" : "  ");
+            }
+            printf("Done.\n");
+        }
+    }
+    return 0;
+// #endif
+}
